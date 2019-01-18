cd9196dacaa4e9cd7ab620166ef7d0dce74e3442
hezi <genesis.alvarez@uamaker.nyc>
2019-01-18 18:23:12 -0500

    data file
    this is the data file with all the variables

---
+++ quizGAdata.py
@@ -0,0 +1,90 @@
+#Initialize local variables
+a1 = int(3) #correct answer for question 1
+c1 = False #check whether user has answered question properly loop
+u1 = int(0) #users answer to question
+q1 = str("""1. What is the biggest channel on YouTube (based on subscribers)?
+1) T-Series
+2) Youtube Gaming
+3) PewDiePie
+4) JustDestiny""") #first question
+
+a2 = int(4) #correct answer for question 2
+c2 = False #checks whether user has answered question
+u2 = int(0) #users answer to second question
+q2 = str("""2. What does PewDiePie posts on his channel?
+1) Meme review
+2) LWIAY (Last Week I Asked You)
+3) You Laugh, You Lose (YLYL)
+4) All of the above""") #string of second question
+
+a3 = int(2) #correct answer for question 3
+c3 = False #check whether user has answered question
+u3 = int(0) #users answer to third question
+q3 = str("""3. Where was PewDiePie born?
+1) America
+2) Sweden
+3) England
+4) Norway""") #third question string
+
+a4 = int(4) #correct answer for fouth question
+c4 = False #check whether user has answered the question
+u4 = int(0) #users answer to fouth question
+q4 = str("""4. What media outlet made an article that PewDiePie is anti-semetic?
+1) Vox
+2) CNN
+3) The Verge
+4) 1 and 3""") #fouth question string
+
+a5 = int(4) #correct answer for fifth question
+c5 = False #check whether user has answered the question
+u5 = int(0) #users answer to fifth question
+q5 = str("""5. How many subscribers does PewDiePie have?
+1) 100 million
+2) 113 million
+3) 66 million
+4) 80 million""") #fifth question string
+
+a6 = int(3) #correct answer for sixth question
+c6 = False #check whether user has answered the question
+u6 = int(0) #users answer to sixth question
+q6 = str("""6. Why did PewDiePie drop out of University?
+1) His grades weren't good
+2) He wanted to focus on Youtube
+3) He just didn't like school
+4) He wanted to move to a different country""") #sixth question string
+
+a7 = int(4) #correct answer for seventh question
+c7 = False #check whether the user has answered the question
+u7 = int(0) #users answer to seventh question
+q7 = str("""7. Who is PewDiePie dating?
+1) No one
+2) Ben Shapiro
+3) The Slavic girl
+4) Marzia""") #seventh question string
+
+a8 = int(3) #correct answer for eigth question
+c8 = False #check whether the user has answered the question
+u8 = int(0) #users answer to eigth question
+q8 = str("""8. What is the political stance of most of his fan base?
+1) Liberal
+2) I don't want to make a generalization
+3) Conservative
+4) Mixed""") #eigth question string
+
+a9 = int(4) #correct answer for ninth question
+c9 = False #check whether the user has answered the question
+u9 = int(0) #users answer to ninth question
+q9 = str("""9. What did Logan Paul do to PewDiePie recently?
+1) He made an Ad out of him
+2) He helped him with the fight with T-Series
+3) Talk about Jake Paul on his Channel
+4) Both 1 and 2""") #ninth question string
+
+a10 = int(3) #correct answer for tenth question
+c10 = False #check whether the user has answered the question
+u10 = int(0) #users answer to tenth question
+q10 = str("""10. Do you like PewDiePie?
+1) No
+2) I don't know him
+3) Yes
+4) Don't ask me personal questions""") #tenth question string
